1df5ccfdb1e15c28aba87e090e1b489ba80a7b58
AaronDai1113 <aaron11388@outlook.com>
2020-11-01 19:51:45 +0800

    111

--- routes/articles.js
+++ routes/articles.js
@@ -6,21 +6,31 @@ router.get('/new',(req,res)=>{
     res.render('articles/new',{article: new Article()});
 });
 
-router.get('/:id',(req,res)=>{
-    res.send(req.params.id);
+router.get('/:slug',async (req,res)=>{    
+    const article= await Article.findOne({slug:req.params.slug});
+    if(article==null){
+        res.redirect('/');
+    }else{
+        res.render('articles/show',{article:article});
+    }
 })
 router.post('/',async (req,res)=>{
-    const article = new Article({
+    let article = new Article({
         title:req.body.title,
         description:req.body.description,
         markdown:req.body.markdown
     })
     try {
         article = await article.save()
-        res.redirect(`/articles/${article.id}`)
+        res.redirect(`/articles/${article.slug}`)
     } catch (error) {
         console.log(error)
         res.render('articles/new',{article: article})
     }
+})
+
+router.delete('/:id',async (req,res)=>{
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
 })
 module.exports=router;
